488b89771a4dd520be79b043093da26192aa54b3
Mark Sargento <iammac.dicoder@gmail.com>
2014-03-26 17:41:39 +0800

    Implement MOL.config initializer

--- lib/mol.rb
+++ lib/mol.rb
@@ -1,1 +1,17 @@
 require 'mol/version'
+
+module MOL
+  autoload :Config, 'mol/config'
+
+  class << self
+    attr_accessor :config
+
+    def config
+      @config ||= Config.new
+    end
+
+    def configure
+      yield(config)
+    end
+  end
+end
--- spec/mol_spec.rb
+++ spec/mol_spec.rb
@@ -5,4 +5,36 @@ describe MOL do
   it "should have a VERSION constant" do
     expect(subject.const_get('VERSION')).to_not be_empty
   end
+
+  describe ".config" do
+    specify { expect(MOL.config).to be_a MOL::Config }
+  end
+
+  describe ".configure" do
+    specify { expect { |b| MOL.configure(&b) }.to yield_control }
+
+    let(:code) { "123" }
+    let(:secret) { "secret" }
+    let(:app_name) { "APP" }
+    let(:currency) { "PHP" }
+
+    before do
+      mol_conf = MOL.configure do |config|
+        config.code = code
+        config.secret = secret
+        config.currency = currency
+        config.app_name = app_name
+        config.sandbox_mode = true
+      end
+    end
+
+    subject { MOL.config }
+
+    specify { expect(subject).to be_a MOL::Config }
+    specify { expect(subject.code).to eq code }
+    specify { expect(subject.secret).to eq secret }
+    specify { expect(subject.app_name).to eq app_name }
+    specify { expect(subject.currency).to eq currency}
+    specify { expect(subject.payout_url).to eql "https://sandbox.api.mol.com/payout/" }
+  end
 end
